fd67cb99c3486cd055d3c32740b4b9d831975619
Gabe Mankowski <gmankow@iastate.edu>
2025-10-10 09:30:49 -0500

    fixed minor bugs, removed don't care values

--- ControlLogic/controlSignals.vhdl
+++ ControlLogic/controlSignals.vhdl
@@ -43,18 +43,18 @@ architecture behavioral of controlSignals is
 
         -- TODO INCLUDE WFI AND CHECK ON DON'T CARE BITS
         
-        o_ALUsrcA <= '1' when (i_Opcode = "0010111" and i_Funct3 = "---" and i_Funct7 = "-------") -- auipc
+        o_ALUsrcA <= '1' when (i_Opcode = "0010111") -- auipc
             else '0';
 
         o_ALUsrcB <= '1' when (i_Opcode = "0010011" and i_Funct3 = "000") or -- addi
-            (i_Opcode = "001001" and i_Funct3 = "111") or -- andi
-            (i_Opcode = "0110111" and i_Funct3 = "---") or -- lui
+            (i_Opcode = "0010011" and i_Funct3 = "111") or -- andi
+            (i_Opcode = "0110111") or -- lui
             (i_Opcode = "0000011" and i_Funct3 = "010") or -- lw
             (i_Opcode = "0010011" and i_Funct3 = "100") or -- xori
             (i_Opcode = "0010011" and i_Funct3 = "110") or -- ori
             (i_Opcode = "0010011" and i_Funct3 = "010") or -- slti
             (i_Opcode = "0010011" and i_Funct3 = "011") or -- sltiu
-            (i_Opcode = "1101111" and i_Funct3 = "---") or -- jal
+            (i_Opcode = "1101111") or -- jal
             (i_Opcode = "1100111" and i_Funct3 = "000") or -- jalr
             (i_Opcode = "0000011" and i_Funct3 = "000") or -- lb
             (i_Opcode = "0000011" and i_Funct3 = "001") or -- lh
@@ -63,28 +63,27 @@ architecture behavioral of controlSignals is
             (i_Opcode = "0010011" and i_Funct3 = "001" and i_Funct7 = "0000000") or -- slli
             (i_Opcode = "0010011" and i_Funct3 = "101" and i_Funct7 = "0000000") or -- srli
             (i_Opcode = "0010011" and i_Funct3 = "101" and i_Funct7 = "0100000") or -- srai
-            (i_Opcode = "0010111" and i_Funct3 = "---" and i_Funct7 = "-------") -- auipc
+            (i_Opcode = "0010111") -- auipc
             else '0';
         
-        o_ALUop <= alu_ADD when (i_Opcode = "0010011" and i_Funct3 = "000" and i_Funct7 = "-------") else -- addi
+        o_ALUop <= alu_ADD when (i_Opcode = "0010011" and i_Funct3 = "000") else -- addi
             alu_ADD when (i_Opcode = "0110011" and i_Funct3 = "000" and i_Funct7 = "0000000") else -- add
-            alu_ADD when (i_Opcode = "0010011" and i_Funct3 = "111" and i_Funct7 = "-------") else -- andi
-            alu_AND when (i_Opcode = "0100011" and i_Funct3 = "000" and i_Funct7 = "-------") else -- sw
-            alu_AND when (i_Opcode = "11001111" and i_Funct3 = "000" and i_Funct7 = "-------") else -- jalr
-            alu_AND when (i_Opcode = "0000011" and i_Funct3 = "000" and i_Funct7 = "-------") else -- lb
-            alu_AND when (i_Opcode = "0000011" and i_Funct3 = "001" and i_Funct7 = "-------") else -- lh
-            alu_AND when (i_Opcode = "0000011" and i_Funct3 = "100" and i_Funct7 = "-------") else -- lbu
-            alu_AND when (i_Opcode = "0000011" and i_Funct3 = "101" and i_Funct7 = "-------") else -- lhu
-            alu_ADD when (i_Opcode = "0010111" and i_Funct3 = "---" and i_Funct7 = "-------") else -- auipc
+            alu_ADD when (i_Opcode = "0100011" and i_Funct3 = "000") else -- sw
+            alu_ADD when (i_Opcode = "1100111" and i_Funct3 = "000") else -- jalr
+            alu_ADD when (i_Opcode = "0000011" and i_Funct3 = "000") else -- lb
+            alu_ADD when (i_Opcode = "0000011" and i_Funct3 = "001") else -- lh
+            alu_ADD when (i_Opcode = "0000011" and i_Funct3 = "100") else -- lbu
+            alu_ADD when (i_Opcode = "0000011" and i_Funct3 = "101") else -- lhu
+            alu_ADD when (i_Opcode = "0010111") else -- auipc
 
             alu_AND when (i_Opcode = "0110011" and i_Funct3 = "111" and i_Funct7 = "0000000") else -- and
-            alu_AND when (i_Opcode = "0010011" and i_Funct3 = "111" and i_Funct7 = "-------") else -- andi
+            alu_AND when (i_Opcode = "0010011" and i_Funct3 = "111") else -- andi
 
             alu_OR when (i_Opcode = "0110011" and i_Funct3 = "110" and i_Funct7 = "0000000") else -- or
-            alu_OR when (i_Opcode = "0010011" and i_Funct3 = "110" and i_Funct7 = "-------") else -- ori
+            alu_OR when (i_Opcode = "0010011" and i_Funct3 = "110") else -- ori
 
             alu_XOR when (i_Opcode = "0110011" and i_Funct3 = "100" and i_Funct7 = "0000000") else -- xor
-            alu_XOR when (i_Opcode = "0010011" and i_Funct3 = "100" and i_Funct7 = "-------") else -- xori
+            alu_XOR when (i_Opcode = "0010011" and i_Funct3 = "100") else -- xori
 
             alu_SLL when (i_Opcode = "0110011" and i_Funct3 = "001" and i_Funct7 = "0000000") else -- sll
             alu_SLL when (i_Opcode = "0010011" and i_Funct3 = "001" and i_Funct7 = "0000000") else -- slli
@@ -93,65 +92,67 @@ architecture behavioral of controlSignals is
             alu_SRL when (i_Opcode = "0010011" and i_Funct3 = "101" and i_Funct7 = "0000000") else -- srli
 
             alu_SUB when (i_Opcode = "0110011" and i_Funct3 = "000" and i_Funct7 = "0100000") else -- sub
-            alu_SUB when (i_Opcode = "1100011" and i_Funct3 = "000" and i_Funct7 = "-------") else -- beq
-            alu_SUB when (i_Opcode = "1100011" and i_Funct3 = "001" and i_Funct7 = "-------") else -- bne
+            alu_SUB when (i_Opcode = "1100011" and i_Funct3 = "000") else -- beq
+            alu_SUB when (i_Opcode = "1100011" and i_Funct3 = "001") else -- bne
 
             alu_SLT when (i_Opcode = "0110011" and i_Funct3 = "010" and i_Funct7 = "0000000") else -- slt
-            alu_SLT when (i_Opcode = "0010011" and i_Funct3 = "010" and i_Funct7 = "-------") else -- slti
-            alu_SLT when (i_Opcode = "1100011" and i_Funct3 = "100" and i_Funct7 = "-------") else -- blt
-            alu_SLT when (i_Opcode = "1100011" and i_Funct3 = "101" and i_Funct7 = "-------") else -- bge
+            alu_SLT when (i_Opcode = "0010011" and i_Funct3 = "010") else -- slti
+            alu_SLT when (i_Opcode = "1100011" and i_Funct3 = "100") else -- blt
+            alu_SLT when (i_Opcode = "1100011" and i_Funct3 = "101") else -- bge
 
-            alu_STLU when (i_Opcode = "0010011" and i_Funct3 = "011" and i_Funct7 = "0000000") else -- sltu
-            alu_STLU when (i_Opcode = "1100011" and i_Funct3 = "110" and i_Funct7 = "-------") else -- bltu
-            alu_STLU when (i_Opcode = "1100011" and i_Funct3 = "111" and i_Funct7 = "-------") else -- bgeu
+            alu_SLTU when (i_Opcode = "0010011" and i_Funct3 = "011" and i_Funct7 = "0000000") else -- sltu
+            alu_SLTU when (i_Opcode = "1100011" and i_Funct3 = "110") else -- bltu
+            alu_SLTU when (i_Opcode = "1100011" and i_Funct3 = "111") else -- bgeu
 
             alu_SRA when (i_Opcode = "0110011" and i_Funct3 = "101" and i_Funct7 = "0100000") else -- sra
             alu_SRA when (i_Opcode = "0010011" and i_Funct3 = "101" and i_Funct7 = "0100000") -- srai
 
-            else "0010"; -- default to ADD
+            else alu_ADD; -- default to ADD
     
-        o_MemWrite <= '1' when (i_Opcode = "0100011" and i_Funct3 = "010" and i_Funct7 = "-------") -- sw
+        o_MemWrite <= '1' when (i_Opcode = "0100011" and i_Funct3 = "010") -- sw
             else '0';
         
-        o_RegWrite <= '0' when (i_Opcode = "0100011" and i_Funct3 = "010" and i_Funct7 = "-------") or -- sw
-            (i_Opcode = "1100011" and i_Funct3 = "000" and i_Funct7 = "-------") or -- beq
-            (i_Opcode = "1100011" and i_Funct3 = "001" and i_Funct7 = "-------") or -- bne
-            (i_Opcode = "1100011" and i_Funct3 = "100" and i_Funct7 = "-------") or -- blt
-            (i_Opcode = "1100011" and i_Funct3 = "101" and i_Funct7 = "-------") or -- bge
-            (i_Opcode = "1100011" and i_Funct3 = "110" and i_Funct7 = "-------") or -- bltu
-            (i_Opcode = "1100011" and i_Funct3 = "111" and i_Funct7 = "-------") -- bgeu
+        o_RegWrite <= '0' when (i_Opcode = "0100011" and i_Funct3 = "010") or -- sw
+            (i_Opcode = "1100011" and i_Funct3 = "000") or -- beq
+            (i_Opcode = "1100011" and i_Funct3 = "001") or -- bne
+            (i_Opcode = "1100011" and i_Funct3 = "100") or -- blt
+            (i_Opcode = "1100011" and i_Funct3 = "101") or -- bge
+            (i_Opcode = "1100011" and i_Funct3 = "110") or -- bltu
+            (i_Opcode = "1100011" and i_Funct3 = "111") -- bgeu
             else '1';
         
-        PCorMemtoReg <= "01" when (i_Opcode = "0000011" and i_Funct3 = "010" and i_Funct7 = "-------") else -- lw
-            "01" when (i_Opcode = "0000011" and i_Funct3 = "000" and i_Funct7 = "-------") else -- lb
-            "01" when (i_Opcode = "0000011" and i_Funct3 = "001" and i_Funct7 = "-------") else -- lh
-            "01" when (i_Opcode = "0000011" and i_Funct3 = "100" and i_Funct7 = "-------") else -- lbu
-            "01" when (i_Opcode = "0000011" and i_Funct3 = "101" and i_Funct7 = "-------") else -- lhu
-            "10" when (i_Opcode = "1101111" and i_Funct3 = "---" and i_Funct7 = "-------") else -- jal
-            "10" when (i_Opcode = "1100111" and i_Funct3 = "000" and i_Funct7 = "-------") -- jalr
+        o_PCorMemtoReg <= "01" when (i_Opcode = "0000011" and i_Funct3 = "010") else -- lw
+            "01" when (i_Opcode = "0000011" and i_Funct3 = "000") else -- lb
+            "01" when (i_Opcode = "0000011" and i_Funct3 = "001") else -- lh
+            "01" when (i_Opcode = "0000011" and i_Funct3 = "100") else -- lbu
+            "01" when (i_Opcode = "0000011" and i_Funct3 = "101") else -- lhu
+            "10" when (i_Opcode = "1101111") else -- jal
+            "10" when (i_Opcode = "1100111" and i_Funct3 = "000") -- jalr
             else "00";
         
-        o_Branch <= '1' when (i_Opcode = "1100011" and i_Funct3 = "000" and i_Funct7 = "-------") or -- beq
-            (i_Opcode = "1100011" and i_Funct3 = "001" and i_Funct7 = "-------") or -- bne
-            (i_Opcode = "1100011" and i_Funct3 = "100" and i_Funct7 = "-------") or -- blt
-            (i_Opcode = "1100011" and i_Funct3 = "101" and i_Funct7 = "-------") or -- bge
-            (i_Opcode = "1100011" and i_Funct3 = "110" and i_Funct7 = "-------") or -- bltu
-            (i_Opcode = "1100011" and i_Funct3 = "111" and i_Funct7 = "-------") -- bgeu
+        o_Branch <= '1' when (i_Opcode = "1100011" and i_Funct3 = "000") or -- beq
+            (i_Opcode = "1100011" and i_Funct3 = "001") or -- bne
+            (i_Opcode = "1100011" and i_Funct3 = "100") or -- blt
+            (i_Opcode = "1100011" and i_Funct3 = "101") or -- bge
+            (i_Opcode = "1100011" and i_Funct3 = "110") or -- bltu
+            (i_Opcode = "1100011" and i_Funct3 = "111") -- bgeu
             else '0';
 
-        o_Jump <= '1' when (i_Opcode = "1101111" and i_Funct3 = "---" and i_Funct7 = "-------") or -- jal
-            (i_Opcode = "1100111" and i_Funct3 = "000" and i_Funct7 = "-------") -- jalr
+        o_Jump <= '1' when (i_Opcode = "1101111") or -- jal
+            (i_Opcode = "1100111" and i_Funct3 = "000") -- jalr
             else '0';
 
-        o_ImmSel <= imm_S_TYPE when (i_Opcode = "0100011" and i_Funct3 = "010" and i_Funct7 = "-------") else -- sw
-            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "000" and i_Funct7 = "-------") else -- beq
-            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "001" and i_Funct7 = "-------") else -- bne
-            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "100" and i_Funct7 = "-------") else -- blt
-            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "101" and i_Funct7 = "-------") else -- bge
-            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "110" and i_Funct7 = "-------") else -- bltu
-            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "111" and i_Funct7 = "-------") else -- bgeu
-            imm_U_TYPE when (i_Opcode = "0110111" and i_Funct3 = "---" and i_Funct7 = "-------") else -- lui
-            imm_U_TYPE when (i_Opcode = "0010111" and i_Funct3 = "---" and i_Funct7 = "-------") else -- auipc
-            imm_UJ_TYPE when (i_Opcode = "1101111" and i_Funct3 = "---" and i_Funct7 = "-------") else -- jal
+        o_ImmSel <= imm_S_TYPE when (i_Opcode = "0100011" and i_Funct3 = "010") else -- sw
+            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "000") else -- beq
+            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "001") else -- bne
+            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "100") else -- blt
+            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "101") else -- bge
+            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "110") else -- bltu
+            imm_SB_TYPE when (i_Opcode = "1100011" and i_Funct3 = "111") else -- bgeu
+            imm_U_TYPE when (i_Opcode = "0110111") else -- lui
+            imm_U_TYPE when (i_Opcode = "0010111") else -- auipc
+            imm_UJ_TYPE when (i_Opcode = "1101111") else -- jal
             else imm_I_TYPE; -- default to I type
+
+end architecture behavioral;
 
