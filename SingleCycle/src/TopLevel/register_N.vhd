b59d2cd33cc900a2c71ef20bc72f75344f8fb8a1
blanef <blanef@iastate.edu>
2025-11-05 13:32:52 -0600

    Added Toolflow to Single Cycle folder

---
+++ SingleCycle/src/TopLevel/register_N.vhd
@@ -0,0 +1,45 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity register_N is
+  generic (
+    N : integer := 32;
+    INIT_VALUE : std_logic_vector(31 downto 0) := (others => '0')
+  );
+  port(i_CLK        : in std_logic;                 -- Clock input
+       i_RST        : in std_logic;                 -- Reset input
+       i_WE         : in std_logic;                 -- Write enable input
+       i_D          : in std_logic_vector(31 downto 0); -- Data value input
+       o_Q          : out std_logic_vector(31 downto 0)  -- Data value output
+       );
+end register_N;
+
+architecture structural of register_N is
+    
+  component dffg
+    generic (INIT_BIT : std_logic := '0'); -- Initial value of the flip-flop
+    port(i_CLK        : in std_logic;     -- Clock input
+         i_RST        : in std_logic;     -- Reset input
+         i_WE         : in std_logic;     -- Write enable input
+         i_D          : in std_logic;     -- Data value input
+         o_Q          : out std_logic);   -- Data value output
+  end component;
+
+  begin 
+
+    -- Generate N instances of dffg
+    gen_dffg : for i in 0 to N-1 generate
+      dffg_inst : dffg
+        generic map (INIT_BIT => INIT_VALUE(i))
+        port map(
+          i_CLK => i_CLK,
+          i_RST => i_RST,
+          i_WE  => i_WE,
+          i_D   => i_D(i),
+          o_Q   => o_Q(i)
+        );
+    end generate gen_dffg;
+
+end structural;
+
+  
