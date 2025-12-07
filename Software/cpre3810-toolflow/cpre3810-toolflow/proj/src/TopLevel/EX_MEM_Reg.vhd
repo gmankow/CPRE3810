6e2cf078af03f9cb01ed9c841d8d2b498d8d7c7a
gmankow <gmankow@iastate.edu>
2025-12-07 15:11:30 -0600

    synth results for Software pipeline

--- Software/cpre3810-toolflow/cpre3810-toolflow/proj/src/TopLevel/EX_MEM_Reg.vhd
+++ Software/cpre3810-toolflow/cpre3810-toolflow/proj/src/TopLevel/EX_MEM_Reg.vhd
@@ -44,7 +44,7 @@ architecture structural of EX_MEM_Reg is
     component register_N
         generic (
             N : integer := 32;
-            INIT_VALUE : std_logic_vector(N-1 downto 0) := (others => '0')
+            INIT_VALUE : std_logic_vector(32-1 downto 0) := (others => '0')
         );
         port(
             i_CLK : in std_logic;
